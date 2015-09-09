ce2b8588cd73b58c185225d61b091062136ab19f
gabriel <gabriel.lucas8502@gmail.com>
2015-09-09 15:33:57 +0200

    + Add UART Serial class definition

---
+++ src/Serial/UARTSerial.cpp
@@ -0,0 +1,14 @@
+/*
+ * UARTSerial.cpp
+ *
+ *  Created on: Aug 28, 2015
+ *      Author: gabriel
+ */
+
+#include "UARTSerial.h"
+
+UARTSerial::UARTSerial() : FDCommunication() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ src/Serial/UARTSerial.h
@@ -0,0 +1,18 @@
+/*
+ * UARTSerial.h
+ *
+ *  Created on: Aug 28, 2015
+ *      Author: gabriel
+ */
+
+#ifndef UARTSERIAL_H_
+#define UARTSERIAL_H_
+
+#include "../IO/FDCommunication.h"
+
+class UARTSerial : public FDCommunication {
+public:
+	UARTSerial();
+};
+
+#endif /* UARTSERIAL_H_ */
