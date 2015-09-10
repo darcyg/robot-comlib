e02f5d0decdf1417dc426e0b780a9a04da940f2f
gabriel <gabriel.lucas8502@gmail.com>
2015-09-10 17:27:05 +0200

    # Fix thread issue # Fix TCPClient bugs

---
+++ src/Socket/Client/TCPClient.cpp
@@ -0,0 +1,66 @@
+/*
+ * TCPClient.cpp
+ *
+ *  Created on: Sep 8, 2015
+ *      Author: gabriel
+ */
+
+#include "TCPClient.h"
+
+TCPClient::TCPClient() {
+	mrunning = false;
+	mthread = nullptr;
+	mevents = nullptr;
+	addFD(this);
+}
+
+TCPClient::~TCPClient() {
+	if(mrunning) {
+		close();
+		mthread->join();
+	}
+	delete(mthread);
+}
+
+void TCPClient::launch(std::string address, int port) {
+
+	try {
+		connect(address, port);
+		if(mevents) mevents->onConnected();
+			mrunning = true;
+			mthread = new std::thread(&TCPClient::privaterun, this);
+	}
+	catch(SocketException &e) {
+		if(mevents) mevents->onConnectionFailed();
+	}
+}
+
+bool TCPClient::isConnected() {
+	return mrunning;
+}
+
+void TCPClient::privaterun() {
+	while(mrunning) {
+
+		listen();
+
+		// Donne la taille de la donnée reçue
+		uint count = getReadSize();
+
+		// Déconnecté
+		if(count == 0) {
+			if(mevents) mevents->onDisconnected();
+			close();
+			remFD(this);
+			mrunning = false;
+		}
+		//Nouveau message
+		else {
+			uint8_t* data = new uint8_t[count];
+
+			read(data, count);
+
+			if(mevents) mevents->onMessageReceived(data, count);
+		}
+	}
+}
