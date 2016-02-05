fea671d3795c2fd5c28ebe743ecc78abcd59fe0b
Gabapps <gabriel.lucas8502@gmail.com>
2016-02-05 23:19:17 +0100

    # Changed tty config

--- src/Serial/UARTSerial.cpp
+++ src/Serial/UARTSerial.cpp
@@ -17,57 +17,39 @@ UARTSerial::~UARTSerial(){
 }
 
 void UARTSerial::open(const std::string& device) throw(IOException) {
-	mfd = ::open(device.c_str(), O_RDWR | O_NOCTTY | O_NONBLOCK);
+	mfd = ::open(device.c_str(), O_RDWR | O_NOCTTY);
 	if (mfd < 0) {
 		throw IOException();
+	}
+	else {
+		init();
 	}
 }
 
 void UARTSerial::init(){
-	//utilisation buffer génériques
-	struct termios tty_attributes;
+	struct termios   options;    // Terminal options
+	int              rc;         // Return value
+	// Get the current options for the port
+	if((rc = tcgetattr(mfd, &options)) < 0){
+		fprintf(stderr, "failed to get attr: %d, %s\n", mfd, strerror(errno));
+	}
 
-	tcgetattr(mfd, &tty_attributes);
-	// Enable receiver
-	cfmakeraw(&tty_attributes);// indispensable pour desactiver caractères de controle!!! ex 0x11
-	tty_attributes.c_cflag |= CREAD;
+	// Set the baud rates to 230400
+	cfsetispeed(&options, B230400);
 
-	// 8 data bit
-	tty_attributes.c_cflag |= CS8;
+	// Set the baud rates to 230400
+	cfsetospeed(&options, B230400);
 
-	// c_iflag
-	// Ignore framing errors and parity errors.
-	tty_attributes.c_iflag |= IGNPAR;
+	cfmakeraw(&options);
+	options.c_cflag |= (CLOCAL | CREAD);   // Enable the receiver and set local mode
+	options.c_cflag &= ~CSTOPB;            // 1 stop bit
+	options.c_cflag &= ~CRTSCTS;           // Disable hardware flow control
+	options.c_cc[VMIN]  = 1;
+	options.c_cc[VTIME] = 2;
 
-	// c_lflag
-	// DISABLE canonical mode.
-	// Disables the special characters EOF, EOL, EOL2,
-	// ERASE, KILL, LNEXT, REPRINT, STATUS, and WERASE, and buffers by lines.
-	tty_attributes.c_lflag &= ~(ICANON);
-
-	// DISABLE this: Echo input characters.
-	tty_attributes.c_lflag &= ~(ECHO);
-
-	// DISABLE this: If ICANON is also set, the ERASE character erases the preceding input
-	// character, and WERASE erases the preceding word.
-	tty_attributes.c_lflag &= ~(ECHOE);
-
-	// DISABLE this: When any of the characters INTR, QUIT, SUSP, or DSUSP are received, generate the corresponding signal.
-	tty_attributes.c_lflag &= ~(ISIG);
-
-	/* polling read : Si data dispo read()
-	 * retourne nb de bytes dispo ou nb bytes demandes.
-	 */
-	// Minimum number of characters for non-canonical read.
-	tty_attributes.c_cc[VMIN] = 0;
-
-	// Timeout in deciseconds for non-canonical read.
-	tty_attributes.c_cc[VTIME] = 0;
-
-	// Set the baud rate
-	cfsetospeed(&tty_attributes, B9600);
-	cfsetispeed(&tty_attributes, B9600);
-	tcsetattr(mfd, TCSANOW, &tty_attributes);
-	usleep(100);
+	// Set the new attributes
+	if((rc = tcsetattr(mfd, TCSANOW, &options)) < 0){
+		fprintf(stderr, "failed to set attr: %d, %s\n", mfd, strerror(errno));
+	}
 }
 
