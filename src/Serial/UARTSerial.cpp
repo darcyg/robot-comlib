cb7e51be5212336e284df9dde5f8df09c8dfd96d
Gabapps <gabriel.lucas8502@gmail.com>
2016-02-06 20:15:54 +0100

    + baudrate setter

--- src/Serial/UARTSerial.cpp
+++ src/Serial/UARTSerial.cpp
@@ -34,8 +34,6 @@ void UARTSerial::init(){
 		fprintf(stderr, "failed to get attr: %d, %s\n", mfd, strerror(errno));
 	}
 
-
-
 	cfmakeraw(&options);
 	options.c_cflag |= (CLOCAL | CREAD);   // Enable the receiver and set local mode
 	options.c_cflag &= ~CSTOPB;            // 1 stop bit
